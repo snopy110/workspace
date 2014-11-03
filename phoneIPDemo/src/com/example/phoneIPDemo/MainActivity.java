b507d3067f6fa326eac245a74fcf4546a406a3a8
snopy110 <snopy110@sina.com>
2014-11-03 11:54:23 +0800

    add phoneIPDemo

---
+++ phoneIPDemo/src/com/example/phoneIPDemo/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.phoneIPDemo;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private String Tag = "MainActivity.class";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView ipTextView = (TextView) findViewById(R.id.IPtext);
+
+		String ipstr = getIP();
+
+		if (ipstr == null) {
+
+			ipTextView.setText("ip is null");
+
+		} else {
+			ipTextView.setText(ipstr);
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private String getWifiIp() {
+		WifiManager wifiManager = (WifiManager) getSystemService(this.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		int ipAddress = wifiInfo.getIpAddress();
+		Log.d(Tag, "int ip " + ipAddress);
+		if (ipAddress == 0)
+			return null;
+		return ((ipAddress & 0xff) + "." + (ipAddress >> 8 & 0xff) + "."
+				+ (ipAddress >> 16 & 0xff) + "." + (ipAddress >> 24 & 0xff));
+	}
+
+	public static String get3GIpAddress() {
+
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface
+					.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf
+						.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()
+							&& inetAddress instanceof Inet4Address) {
+						// if (!inetAddress.isLoopbackAddress() && inetAddress
+						// instanceof Inet6Address) {
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean isWifiConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mWiFiNetworkInfo = mConnectivityManager
+					.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			if (mWiFiNetworkInfo != null) {
+				return mWiFiNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+	
+	public boolean isMobileConnected(Context context) {  
+	    if (context != null) {  
+	        ConnectivityManager mConnectivityManager = (ConnectivityManager) context  
+	                .getSystemService(Context.CONNECTIVITY_SERVICE);  
+	        NetworkInfo mMobileNetworkInfo = mConnectivityManager  
+	                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);  
+	        if (mMobileNetworkInfo != null) {  
+	            return mMobileNetworkInfo.isAvailable();  
+	        }  
+	    }  
+	    return false;  
+	}
+	
+	public String getIP(){
+		String ip = null;
+		
+		if (isWifiConnected(this)) {
+			ip = getWifiIp();
+		} else if(isMobileConnected(this)){
+			ip = get3GIpAddress();
+		}
+		
+		return ip;
+	}
+
+}
